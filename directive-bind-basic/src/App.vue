ac37b7056f516cf7e7b6a2bf3329c3b13bbaf6c7
ogata <ogata@beaglee.com>
2022-10-30 22:15:26 +0900

    4.1 データバインディングのディレクティブ

---
+++ directive-bind-basic/src/App.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const url = ref("https://vuejs.org/");
+
+const isSendButtonDisabled = ref(true);
+
+const widthOrHeight = ref("height");
+const widthOrHeightValue = ref(100);
+
+const imgAttributes = ref({
+  src: "/images/logo.svg",
+  alt: "Vueのロゴ",
+  width: 75,
+  height: 75,
+});
+</script>
+
+<template>
+  <p><a v-bind:href="url" target="_blank">Vue.jsのサイト</a></p>
+  <p><a :href="url" target="_blank">Vue.jsのサイト(省略形)</a></p>
+  <p>
+    <a v-bind:href="url + 'guide/introduction.html'" target="_blank">
+      Vue.jsのガイドページ
+    </a>
+  </p>
+  <p>
+    <button type="button" v-bind:disabled="isSendButtonDisabled">送信</button>
+  </p>
+  <p>
+    <img
+      alt="VueLogo"
+      src="./assets/logo.svg"
+      v-bind:[widthOrHeight]="widthOrHeightValue"
+    />
+  </p>
+  <p>
+    <img v-bind="imgAttributes" />
+  </p>
+  <p>
+    <img v-bind="imgAttributes" title="ロゴです！" />
+  </p>
+  <p>
+    <img v-bind="imgAttributes" alt="ロゴです！" />
+  </p>
+</template>
---
+++ directive-style/src/App.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { ref, computed } from "vue";
+
+const msg = ref("こんにちは！世界");
+const msgTextRed = ref("red");
+const msgTextColor = ref("while");
+const msgBgColor = ref("black");
+
+const msgStyles = ref({
+  color: "white",
+  backgroundColor: "black",
+});
+const msgStyles2 = ref({
+  fontSize: "24pt",
+});
+const msgStyles3 = ref({
+  color: "pink",
+  fontSize: "24pt",
+});
+const textSize = computed((): string => {
+  const size = Math.round(Math.random() * 25) + 10;
+  return `${size}`;
+});
+</script>
+
+<template>
+  <p v-bind:style="{ color: msgTextRed }">
+    {{ msg }}
+  </p>
+  <p v-bind:style="{ color: 'pink' }">
+    {{ msg }}
+  </p>
+  <p v-bind:style="{ fontSize: textSize }">
+    {{ msg }}
+  </p>
+  <p v-bind:style="{ color: msgTextColor, backgroundColor: msgBgColor }">
+    {{ msg }}
+  </p>
+  <p v-bind:style="{ color: msgTextColor, 'background-color': msgBgColor }">
+    {{ msg }}
+  </p>
+  <p v-bind:style="msgStyles">{{ msg }}</p>
+  <p v-bind:style="[msgStyles, msgStyles2]">{{ msg }}</p>
+  <p v-bind:style="[msgStyles, msgStyles3]">{{ msg }}</p>
+  <p v-bind:style="[msgStyles3, msgStyles]">{{ msg }}</p>
+</template>
