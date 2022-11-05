a4624ade4cfc1086d61585cb96f9a5be86985eee
ogata <ogata@beaglee.com>
2022-11-05 20:54:14 +0900

    8.3 親から子へのコンポーネント間通信

---
+++ components-props/src/App.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { ref } from "vue"
+import OneInfo from "./components/OneInfo.vue"
+
+const propsTitle = ref("発生した乱数");
+const rand = Math.round(Math.random() * 100);
+const propsContent = ref(rand);
+</script>
+
+<!-- <template>
+  <h1>Props基礎</h1>
+  <section>
+    <h2>属性に直接記述</h2>
+    <OneInfo title="Propsの利用" content="子コンポーネントにデータを渡すにはPropsを利用する" />
+  </section>
+</template> -->
+
+<template>
+  <h1>Props基礎</h1>
+  <section>
+    <h2>属性に直接記述</h2>
+    <OneInfo v-bind:title="propsTitle" v-bind:content="propsContent" />
+  </section>
+</template>
+
+
+<style>
+section {
+  border: blue 1px solid;
+  margin: 10px;
+}
+</style>
