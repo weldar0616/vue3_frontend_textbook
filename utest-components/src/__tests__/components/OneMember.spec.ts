156317632425e1289111b8e4697cda9d832f5023
ogata <ogata@beaglee.com>
2022-11-23 21:06:03 +0900

    13.4 コンポーネント間通信のテスト

---
+++ utest-components/src/__tests__/components/OneMember.spec.ts
@@ -0,0 +1,52 @@
+import {describe, test, expect} from "vitest";
+import { mount} from "@vue/test-utils";
+import OneMember from "@/components/OneMember.vue";
+
+describe("OneMemberのテスト", () => {
+  test("備考データがある場合のPropsのテスト", () => {
+    const propsData = {
+      id: 22458,
+      name: "中野三郎",
+      email: "mue@wow.com",
+      points: 200,
+      note: "すばらしい！"
+    };
+    const wrapper = mount(OneMember, {props: propsData});
+    const actualText = wrapper.text();
+    expect(actualText).toContain(String(propsData.id));
+    expect(actualText).toContain(propsData.name);
+    expect(actualText).toContain(propsData.email);
+    expect(actualText).toContain(String(propsData.points));
+    expect(actualText).toContain(propsData.note);
+  });
+  test("備考データがない場合のPropsのテスト", () => {
+    const propsData = {
+      id: 55148,
+      name: "江口四郎",
+      email: "egu@chi.com",
+      points: 300,
+    };
+    const wrapper = mount(OneMember, {props: propsData});
+    const actualText = wrapper.text();
+    expect(actualText).toContain(String(propsData.id));
+    expect(actualText).toContain(propsData.name);
+    expect(actualText).toContain(propsData.email);
+    expect(actualText).toContain(String(propsData.points));
+    expect(actualText).toContain("--");
+  });
+  test("[ポイント加算]ボタンクリックのテスト (Emitのテスト)", async () => {
+    const propsData = {
+      id: 22458,
+      name: "中野三郎",
+      email: "mue@wow.com",
+      points: 200,
+      note: "すばらしい！"
+    };
+    const wrapper = mount(OneMember, {props: propsData});
+    await wrapper.get("button").trigger("click");
+    const incrementPointEvent = wrapper.emitted("incrementPoint");
+    expect(incrementPointEvent).toHaveLength(1);
+    const expectedIncrementPointEvent = [[propsData.id]];
+    expect(incrementPointEvent).toEqual(expectedIncrementPointEvent);
+  });
+})
