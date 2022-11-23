708ce00f9f0696b14c447cd0433deeeff9c38ab4
ogata <ogata@beaglee.com>
2022-11-23 19:44:59 +0900

    13.2 コンポーネントのテストの基本

---
+++ utest-basic/src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { ref, computed } from "vue";
+import { divideTwoNums } from "./functions";
+
+const num1 = ref(6);
+const num2 = ref(3);
+const ans = computed((): number => divideTwoNums(num1.value, num2.value));
+</script>
+
+<template>
+  <p>{{ num1 }}÷{{ num2 }}={{ ans }}</p>
+</template>
--- utest-basic/src/functions.ts
+++ utest-basic/src/functions.ts
@@ -1,3 +1,3 @@
-export function devideTwoNums(num1: number, num2: number): number {
+export function divideTwoNums(num1: number, num2: number): number {
   return num1 / num2;
 }
