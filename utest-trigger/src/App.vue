02bfe1a8114727d68de2c32964d5d05029cd304e
ogata <ogata@beaglee.com>
2022-11-23 20:19:16 +0900

    13.3 コンポーネントのテストの応用 trigger

---
+++ utest-trigger/src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import {ref} from "vue";
+
+const isVisible = ref(false);
+const onShowButtonClick = (): void => {
+  isVisible.value = true;
+};
+</script>
+
+<template>
+  <p>
+    隠し要素を
+    <button data-testid="showButton" v-on:click="onShowButtonClick">表示</button>
+  </p>
+  <p v-if="isVisible" data-testid="invisible">表示されました！</p>
+</template>
